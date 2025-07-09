c9563b13de946ddfca509c600ea139147990095b
JakubHempel <kubahem.13@gmail.com>
2025-07-09 14:27:12 +0200

    Change the way to show maps

--- create_map.py
+++ create_map.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import geemap.foliumap as geemap
 from folium import plugins
 
@@ -23,25 +24,36 @@ vis_params = {
     'AWEI': {'min': -1, 'max': 1, 'palette': ['#f5f5dc', '#ffffcc', '#a1dab4', '#41b6c4', '#225ea8'], 'breaks': [-2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0]}
 }
 
+
 def show_map(cache_image, layer_name, index_name):
     Map = geemap.Map(
         layer_ctrl=True, basemap="HYBRID", control_scale=True
     )
     minimap = plugins.MiniMap()
     Map.add_child(minimap)
-    Map.addLayer(cache_image, {'min': 0, 'max': 0.3, 'bands': ['B4', 'B3', 'B2'], 'gamma': 1.3}, f"RGB - {layer_name}")
+    #Map.addLayer(cache_image, {'min': 0, 'max': 0.3, 'bands': ['B4', 'B3', 'B2'], 'gamma': 1.3}, f"RGB - {layer_name}")
     Map.addLayer(cache_image.select(index_name), vis_params[index_name], f"{index_name} - {layer_name}")
 
     if index_name in ['CDOM', 'DOC']:
         label_name = f"{index_name} Colorbar [mg/l]"
     elif index_name == 'Cyanobacteria':
         label_name = f"{index_name} Colorbar [10^3 cell/ml]"
-    elif index_name == 'Turbidity':
-        label_name = f"{index_name} Colorbar [NTU]"
     else:
         label_name = f"{index_name} Colorbar"
 
-    Map.add_colorbar(vis_params[index_name], label=label_name)
+    #Map.add_colorbar(vis_params[index_name], label=label_name)
 
-    Map.setCenter(19.93, 50.05, 14)
-    Map.to_streamlit(height=800)
+    Map.add_colormap(width=3, height=0.15, vmin=vis_params[index_name]['min'],
+                     vmax=vis_params[index_name]['max'],
+                     palette=vis_params[index_name]['palette'],
+                     label=index_name, label_size=8, bg_color='#FFFFFFD9',
+                     orientation='horizontal', position=(53, 4))
+
+    Map.setCenter(19.94, 50.05, 14)
+    st.markdown(
+        "<div style='text-align: right; font-size: 0.90em; color: gray;'>"
+        "💡 You can toggle layer visibility using the layer control pane in the upper-right corner of the map."
+        "</div>",
+        unsafe_allow_html=True
+    )
+    Map.to_streamlit(height=750)
