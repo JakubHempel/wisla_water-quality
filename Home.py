6754014307dc7e5e0e487a356c67f94a5d247a4b
JakubHempel <kubahem.13@gmail.com>
2025-07-11 16:20:54 +0200

    Just reformating the code

--- Home.py
+++ Home.py
@@ -8,9 +8,11 @@ try:
 except ImportError:
     from io import StringIO
 
+
 @st.cache_data
 def ee_authenticate(token_name="EARTHENGINE_TOKEN"):
     geemap.ee_initialize(token_name=token_name)
+
 
 ee_authenticate(token_name="EARTHENGINE_TOKEN")
 
